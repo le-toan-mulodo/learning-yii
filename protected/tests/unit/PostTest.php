6c5d2e31297d49d88f7970f5ce3e74e20224008a
Toan .A Le <le.toan@mulodo.com>
2013-10-08 15:03:07 +0700

    Update PostTest.php

--- protected/tests/unit/PostTest.php
+++ protected/tests/unit/PostTest.php
@@ -2,12 +2,13 @@
 
 class PostTest extends CDbTestCase
 {
-	public $fixtures=array(
-		'posts'=>'Post',
-	);
+	// public $fixtures=array(
+	// 	'posts'=>'Post',
+	// );
 
 	public function testCreate()
 	{
-
+		$a = "hello";
+		$this->assertEquals("hello",$a);
 	}
 }
